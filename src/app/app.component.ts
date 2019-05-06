cf321d69893afe4e8d53a2e07ed8d15093880856
Cesar Tolosa Franco <cesar.tolosa.franco@gmail.com>
2019-05-06 16:18:21 -0500

    chore(refactor): removes useless pattern validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import * as moment from 'moment';
@@ -10,7 +10,7 @@ import * as moment from 'moment';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  endDateControl = new FormControl('', Validators.pattern('dddd-dd-ddTdd:dd:dd'));
+  endDateControl = new FormControl();
   endDate$: Observable<Date> = this.endDateControl.valueChanges.pipe(
     tap(() => this.endDateReached = false),
     map(value => moment(value).toDate()), // moment used for cross-browser date compatibility
